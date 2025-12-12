d4cebf2366119174f7ec87c93b306b6c1aee3aa6
marwa <marwaessanousy20@gmail.com>
2025-12-12 13:35:19 +0000

    l

--- Faculty_Management/Etudiant_Service/src/main/java/dcc/etudiant_service/FeignClient/FiliereClient.java
+++ Faculty_Management/Etudiant_Service/src/main/java/dcc/etudiant_service/FeignClient/FiliereClient.java
@@ -2,6 +2,7 @@ package dcc.etudiant_service.FeignClient;
 
 
 import dcc.etudiant_service.DTO.Filiere;
+import dcc.etudiant_service.configuration.FeignClientConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,12 +10,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
 @FeignClient(
-        name = "Filiere-Service",
-        url = "http://localhost:8081/v1/filieres" // URL de  microservice filiere
+        name = "FILIERE-SERVICE",
+        configuration = FeignClientConfig.class// URL de  microservice filiere
 )
 public interface FiliereClient {
 
-    @GetMapping("/{id}")
+    @GetMapping("/v1/filieres/{id}")
     public Filiere getFiliereById(@PathVariable("id") Integer id);
 
 
